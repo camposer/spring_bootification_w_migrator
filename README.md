e9f76d743f015f2a8a01cb59e80b95850a23caae
Rodolfo Campos <crodolfo@vmware.com>
2020-07-02 17:23:14 +0200

    migrator-result: Add result of executing bootifier.sh in the project root directory

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ This repo contains the [Spring Bootification Example](https://github.com/campose
 
 Starting point: The initial commit uses the [0-prepare](https://github.com/camposer/spring_bootification_example/tree/0-prepare) branch
 
+## Steps
 
+1. migrator-result. Contains the results of executing bootifier.sh in the home directory
---
+++ src/main/java/SpringHibernateExampleApp.java
@@ -0,0 +1,12 @@
+package ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateExampleApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringHibernateExampleApp.class, args);
+  }
+}
